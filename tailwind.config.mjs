866a2fc72c6c0e9f6d767ae4e944321376af0f61
MartL7 <latiapelitos123@gmail.com>
2024-07-24 14:12:35 -0600

    😸 Added new dependences and conigurate tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,16 @@
+import animations from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				'Dancing': ['Dancing Script Variable', 'cursive'],
-			},
+				'Merri': ['Merriweather Variable', 'serif'],
+				'Oswald': ['Oswald Variable', 'sans-serif'],
+				'Dancing': ['Dancing Script Variable', 'Oswald Variable'],
+				'Public': ['Public Sans Variable', 'sans-serif']
+			},	
 			colors: {
 				primary: "var(--color-primary)",
 				secondary: "var(--color-secondary)",
@@ -15,5 +20,5 @@ export default {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
